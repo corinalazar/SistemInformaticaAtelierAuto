beadadbdbac14dc0e8074220664c8a56794f441e
Corina <Corina@LAPTOP-UD9CV3NA>
2026-01-11 00:01:20 +0200

    add ef models and database context

---
+++ SistemInformaticaAtelierAuto.Web/Data/SistemInformaticaAtelierAutoWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemInformaticaAtelierAuto.Web.Models;
+
+namespace SistemInformaticaAtelierAuto.Web.Data
+{
+    public class SistemInformaticaAtelierAutoWebContext : DbContext
+    {
+        public SistemInformaticaAtelierAutoWebContext (DbContextOptions<SistemInformaticaAtelierAutoWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemInformaticaAtelierAuto.Web.Models.Client> Client { get; set; } = default!;
+    }
+}
---
+++ SistemInformaticaAtelierAuto.Web/Pages/Clients/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SistemInformaticaAtelierAuto.Web.Data;
+using SistemInformaticaAtelierAuto.Web.Models;
+
+namespace SistemInformaticaAtelierAuto.Web.Pages.Clients
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SistemInformaticaAtelierAuto.Web.Data.SistemInformaticaAtelierAutoWebContext _context;
+
+        public IndexModel(SistemInformaticaAtelierAuto.Web.Data.SistemInformaticaAtelierAutoWebContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Client> Client { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Client = await _context.Client.ToListAsync();
+        }
+    }
+}
