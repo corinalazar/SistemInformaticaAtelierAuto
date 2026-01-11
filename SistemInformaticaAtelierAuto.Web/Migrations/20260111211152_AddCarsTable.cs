657567d17fe464dbbfcd099c2ac22ec27141d66c
Corina <Corina@LAPTOP-UD9CV3NA>
2026-01-11 23:18:24 +0200

    final

---
+++ SistemInformaticaAtelierAuto.Web/Migrations/20260111211152_AddCarsTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemInformaticaAtelierAuto.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCarsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
